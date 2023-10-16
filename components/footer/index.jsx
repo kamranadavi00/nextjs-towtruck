a825324a8f5763f0b598c392b31b13a6ec9055cb
kamranadavi <kamranadavi1378@gmail.com>
2023-10-16 17:15:17 +0330

    remove comment in footer

--- components/footer/index.jsx
+++ components/footer/index.jsx
@@ -7,23 +7,7 @@ import { BsFillHouseFill, BsFillTelephoneOutboundFill } from "react-icons/bs";
 const Footer = () => {
   return (
     <div className="text-center flex flex-col items-center justify-center  bg-zinc-500   container mx-auto ">
-      {/* <div className=' flex items-stretch bg-slate-950  p-20 text-white container mx-auto '>
-  <div>
-    <ul >
-      <li> امداد خودرو</li>
-      <li>امداد خودرو غرب</li>
-      <li>مکانیکی سیار</li>
-    </ul >
-  </div>
-  <div>
-    <ul>
-      <li> امداد خودرو</li>
-      <li>امداد خودرو غرب</li>
-      <li>مکانیکی سیار</li>
-    </ul>
-  </div>
-
-  </div> */}
+      
       <div>تمامی حقوق این سایت متعلق به امداد خودرو غرب میباشد.</div>
       <div>
         طراحی سایت و سئو توسط{" "}
